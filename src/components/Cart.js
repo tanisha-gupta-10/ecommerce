f980c7b23685d0f6014ce802c4ed873ebb7de2b6
Tanisha gupta <tanishagupta1007@gmail.com>
2025-01-03 15:21:01 +0530

    cart for mobile handled

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Cart({ handleShowCart, cart, handleRemoveCart, cartPrice }) {
+export default function Cart({ handleShowCart, cart, handleRemoveCart, cartPrice, isMobile }) {
 
     // console.log(cart);
     return (
-        <div className="cart-cont">
+        <div className="cart-cont" style={{width: isMobile? '80%' : '30%'}}>
             <div className="top-part" style={{ alignItems: 'center', justifyContent: 'space-between', borderBottom: '2px solid #f2f2f2' }}>
                 <h2 style={{ margin: 0 }}>Your Cart</h2>
                 <img src="https://icons.veryicon.com/png/o/miscellaneous/medium-thin-linear-icon/cross-23.png" style={{ width: '30px' }} onClick={handleShowCart} />
@@ -15,13 +15,13 @@ export default function Cart({ handleShowCart, cart, handleRemoveCart, cartPrice
 
                 {cart.map((cartItem) => <div key={cartItem.id} className='cartItem'>
                     <div className='product-detail'>
-                        <img src={cartItem.image} alt="Product" />
+                        <img src={cartItem.image} alt="Product" style={{width : isMobile ? '70px' : '100px'}} />
                         <div>
                             <h4>{cartItem.title}</h4>
                             <p>Rs.{cartItem.price} x {cartItem.quantity}</p>
                         </div>
                     </div>
-                    <button className='btn btn-danger' onClick={() => handleRemoveCart(cartItem.id, cartItem.quantity)}>Remove</button>
+                    <button className={`btn btn-danger ${isMobile? 'btn-sm' : ''}`} onClick={() => handleRemoveCart(cartItem.id, cartItem.quantity)}>x</button>
                 </div>)}
 
             </div> : <div class="alert alert-primary text-center" role="alert">
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,7 @@ function Main({ products, loading, addToFvrts, fvrt, add_to_cart, isMobile, hand
                     {products.map(product => <Video key={product.id} isMobile={isMobile} product={product} addToFvrts={addToFvrts} fvrt={fvrt} add_to_cart={add_to_cart}  quantity={quantity}
           handleQuantityChange={handleQuantityChange}  />)}
                 </div>}
-            {showCart ? <Cart key={cart.length} handleShowCart={handleShowCart} cart={cart} handleRemoveCart={handleRemoveCart} cartPrice={cartPrice}/> : null}
+            {showCart ? <Cart key={cart.length} handleShowCart={handleShowCart} cart={cart} handleRemoveCart={handleRemoveCart} cartPrice={cartPrice} isMobile={isMobile}/> : null}
 
         </>
     );
